d529efddf723b95b70b424e2ec214587a9ab302d
ddahan <david.dahan3@gmail.com>
2023-10-07 16:56:15 +0000

    url admin ok

--- dj_config/urls.py
+++ dj_config/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# import debug_toolbar
 
 urlpatterns = [
-    path("dj_admin/", admin.site.urls),
+    # Django Admin dashboard
+    path(f"{settings.PREFIX_URL_ADMIN}_admin/", admin.site.urls),
 ]
