a4a0347c46c3821436fa57d732b2ae8ad5ade6fa
ddahan <david.dahan3@gmail.com>
2023-10-08 11:58:26 +0000

    minor

--- urls.py
+++ urls.py
@@ -1,9 +1,6 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-# import debug_toolbar
 
 urlpatterns = [
     # Django Admin dashboard
