727c1ef9134a4b7fb9a94f54017be931910b2a50
ddahan <david.dahan3@gmail.com>
2023-10-12 16:04:13 +0000

    minor fix

--- dj_apps/core/tests/test_poc.py
+++ dj_apps/core/tests/test_poc.py
@@ -1,7 +1,25 @@
+from django.contrib.auth import get_user_model
+
+import pytest
+
+from badges.models.badge import Badge
+
+User = get_user_model()
+
+
 def test_a():
     assert 4 == 4
 
 
-# This test is designed to fail for demonstration purposes.
-def test_b():
-    assert 3 == 4
+@pytest.mark.django_db
+def test_my_user():
+    badge = Badge.objects.create()
+    print(badge.sid)
+    assert Badge.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_my_user_2():
+    badge = Badge.objects.create()
+    print(badge.sid)
+    assert Badge.objects.count() == 1
