18cb73ebbe1a37bcc9f40a59df37d1fd30dc4df3
ddahan <david.dahan3@gmail.com>
2023-12-01 18:04:42 +0000

    bug fix

--- dj_apps/profiles/tests/factories.py
+++ dj_apps/profiles/tests/factories.py
@@ -10,7 +10,7 @@ class UserFactory(factory.django.DjangoModelFactory):
         model = User
 
     email = factory.LazyAttribute(lambda obj: f"{obj.first_name}@fakemail.com")
-    title = "M"
+    title = "MR"
     first_name = factory.Sequence(lambda n: f"User_{n + 1}")
     last_name = "fake"
 
